4dd258ec92867629d0d267783ffe9de0c29dd094
hoangkimminh <minhhk0312.uet@gmail.com>
2020-05-12 22:00:27 +0700

    [contribute/c3] Format code

--- 15/Memrise-App/App.js
+++ 15/Memrise-App/App.js
@@ -1,65 +1,56 @@
 import "react-native-gesture-handler";
-import React from 'react';
-import { StyleSheet, Text, View ,Button} from 'react-native';
+import React from "react";
+import { StyleSheet, Text, View, Button } from "react-native";
 
 import { NavigationContainer } from "@react-navigation/native";
 import { createStackNavigator } from "@react-navigation/stack";
 
-import Courses from './screens/Courses' ;
-import ListWord from './screens/ListWords';
-import WordDetail from './screens/WordDetail';
-import AddMem from './screens/AddMem';
-import Review from './screens/Review';
+import Courses from "./screens/Courses";
+import ListWord from "./screens/ListWords";
+import WordDetail from "./screens/WordDetail";
+import AddMem from "./screens/AddMem";
+import Review from "./screens/Review";
+import { ROUTES } from "./constants/routes";
 
 const Stack = createStackNavigator();
 
 export default function App() {
   return (
-    <NavigationContainer  style = {styles.container}>
-      <Stack.Navigator initialRouteName="Courses"  > 
-       
-       <Stack.Screen name="Courses" component={Courses} 
-        options={{ 
-          title: 'Course' ,
-          headerStyle:{
-            backgroundColor:'#0ab',
-          },
-          headerRight: () => (
-            <Button
-              onPress={() => alert('This is a button!')}
-              title="Info"
-              color="#fff"
-            />
-          )
+    <NavigationContainer style={styles.container}>
+      <Stack.Navigator initialRouteName={ROUTES.Courses}>
+        <Stack.Screen
+          name={ROUTES.Courses}
+          component={Courses}
+          options={{
+            title: ROUTES.Courses,
+            headerStyle: {
+              backgroundColor: "#0ab",
+            },
+            headerRight: () => (
+              <Button
+                onPress={() => alert("This is a button!")}
+                title="Info"
+                color="#fff"
+              />
+            ),
+          }}
+          // initialParams={{ itemId: 42 }}
+        />
 
-        }}
-        // initialParams={{ itemId: 42 }}
-        
-        /> 
-
-
-        <Stack.Screen name ='ListWord' component={ListWord}
-            options ={{
-              title: 'List Words' ,
-            }}
-        /> 
-        <Stack.Screen name ='WordDetail' component={WordDetail} /> 
-        <Stack.Screen name ='AddMem' component={AddMem} /> 
-        <Stack.Screen name ='Review' component={Review} /> 
+        <Stack.Screen name={ROUTES.ListWord} component={ListWord} />
+        <Stack.Screen name={ROUTES.WordDetail} component={WordDetail} />
+        <Stack.Screen name={ROUTES.AddMem} component={AddMem} />
+        <Stack.Screen name={ROUTES.Review} component={Review} />
       </Stack.Navigator>
-      
-  </NavigationContainer>
+    </NavigationContainer>
   );
-
 }
-
-
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'flex-start',
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "flex-start",
   },
 });
---
+++ 15/Memrise-App/constants/routes.js
@@ -0,0 +1,7 @@
+export const ROUTES = {
+  Courses: "Các bài học",
+  ListWord: "Danh sách từ",
+  WordDetail: "Thông tin từ",
+  AddMem: "Thêm thành viên",
+  Review: "Thực hành",
+};
